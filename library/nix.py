e2456373720d9c5a2f89a730514fe0d1c76e82ea
Markus Binsteiner <makkus@gmail.com>
2017-02-11 13:00:44 +1300

    Added update tasks.

--- library/nix.py
+++ library/nix.py
@@ -17,17 +17,37 @@ import os
 from ansible.module_utils.basic import *
 
 
-NIX_PATH = os.environ['HOME'] + "/.nix-profile/bin/nix-env"
+NIX_PATH = os.path.join(os.environ['HOME'], "/.nix-profile/bin")
+NIX_ENV_PATH = os.path.join(NIX_PATH, "nix-env")
+NIX_CHANNEL_PATH = os.path.join(NIX_PATH, "nix-channel")
 
 def query_package(module, name, state="present"):
     if state == "present":
-        cmd = "{} -q {}".format(NIX_PATH, name)
+        cmd = "{} -q {}".format(NIX_ENV_PATH, name)
         rc, stdout, stderr = module.run_command(cmd, check_rc=False)
 
         if rc == 0:
             return True
 
         return False
+
+def update_cache():
+
+    cmd = "{} --update".format(NIX_CHANNEL_PATH)
+    rc, stdout, stderr = module.run_command(cmd, check_rc=False)
+    if rc != 0:
+        module.fail_json(msg="failed to update cache: {}".format(stderr))
+
+    module.exit_json(changed=True, msg="Updated nix cache.")
+
+def upgrade_packages():
+
+    cmd = "{} --upgrade".format(NIX_ENV_PATH)
+    rc, stdout, stderr = module.run_command(cmd, check_rc=False)
+    if rc != 0:
+        module.fail_json(msg="failed to upgrade packages: {}".format(stderr))
+
+    module.exit_json(changed=True, msg="Upgraded nix packages.")
 
 def install_packages(module, packages):
     install_c = 0
@@ -45,7 +65,7 @@ def install_packages(module, packages):
         if query_package(module, package):
             continue
 
-        cmd = "{} -i {}".format(NIX_PATH, package)
+        cmd = "{} -i {}".format(NIX_ENV_PATH, package)
         rc, stdout, stderr = module.run_command(cmd, check_rc=False)
 
         if rc != 0:
@@ -61,13 +81,16 @@ def install_packages(module, packages):
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            name=dict(aliases=['pkg']),
+            name=dict(aliases=['pkg', 'package']),
+            upgrade = dict(default=False, type='bool'),
+            update_cache = dict(default=False, aliases=['update-cache'], type='bool')
             state=dict(default='present', choices=['present', 'installed', 'absent', 'removed'])),
-        required_one_of=[['name']],
+        required_one_of=[['name', 'upgrade', 'update_cache']],
+        mutually_exclusive=[['name', 'upgrade']],
         supports_check_mode=True)
 
-    if not os.path.exists(NIX_PATH):
-        module.fail_json(msg="cannot find nix-env, looking for %s" % (NIX_PATH))
+    if not os.path.exists(NIX_ENV_PATH):
+        module.fail_json(msg="cannot find nix-env, looking for %s" % (NIX_ENV_PATH))
 
     p = module.params
 
@@ -77,8 +100,16 @@ def main():
     elif p['state'] in ['absent', 'removed']:
         p['state'] = 'absent'
 
+    if p['update_cache']:
+        update_cache()
+
+    if p['upgrade']:
+        upgrade_packages()
+
     if p['name']:
         pkgs = p['name'].split(',')
+
+        if p['update_cache']
 
         if p['state'] == 'present':
             install_packages(module, pkgs)
